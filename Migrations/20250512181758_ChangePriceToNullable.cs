c339e3a9a84e47b9dc1158dd7af1bb7a4c7d940f
Joshua <st10296167@vcconnect.edu.za>
2025-05-12 20:22:36 +0200

    New migration to fix product price validation

---
+++ Migrations/20250512181758_ChangePriceToNullable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JoshuaWood_ST10296167_PROG7311_POE.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePriceToNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
