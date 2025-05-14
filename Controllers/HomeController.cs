4a2c5d0774c81e59e2e86e2129d33ee8fb186857
Joshua <st10296167@vcconnect.edu.za>
2025-05-14 17:53:59 +0200

    Improved index user data handling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using JoshuaWood_ST10296167_PROG7311_POE.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,23 +7,42 @@ namespace JoshuaWood_ST10296167_PROG7311_POE.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
         // Controller
         //------------------------------------------------------------------------------------------------------------------------------------------//
-        public HomeController()
+        public HomeController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
-
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
         //------------------------------------------------------------------------------------------------------------------------------------------//
 
         // Views
         //------------------------------------------------------------------------------------------------------------------------------------------//
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            if (User.Identity == null || !User.Identity.IsAuthenticated)
+            // Check if user is authenticated
+            if (User?.Identity == null || !User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Login", "User");
             }
+
+            var user = await _userManager.GetUserAsync(User);
+
+            // If user is null, sign them out
+            if (user == null)
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("Login", "User");
+            }
+
+            // Add user info for use in Index view
+            ViewData["FirstName"] = user.FirstName ?? "User";
+            ViewData["IsEmployee"] = await _userManager.IsInRoleAsync(user, "Employee");
+            ViewData["IsFarmer"] = await _userManager.IsInRoleAsync(user, "Farmer");
+
             return View();
         }
         //------------------------------------------------------------------------------------------------------------------------------------------//
